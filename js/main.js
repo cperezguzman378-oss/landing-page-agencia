cdbe76a0dd89d07e91bcf6e81b0a7b449063690d
Josías <josiasmonge23@gmail.com>
2026-06-19 19:10:21 -0600

    Version inicial: Landing page Impulso Digital SV finalizada

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const btnHamburguesa = document.getElementById('btn-hamburguesa');
+const menuNavegacion = document.getElementById('menu-navegacion');
+
+btnHamburguesa.addEventListener('click', () => {
+    console.log("El botón fué presionado");
+    menuNavegacion.classList.toggle('activo');
+    console.log("Clases actuales del menú:", menuNavegacion.className);
+});
+
+const enlacesMenu = document.querySelectorAll('.menu-navegacion a');
+
+enlacesMenu.forEach(enlace => {
+    enlace.addEventListener('click', () => {
+        menuNavegacion.classList.remove('activo');
+    });
+});
+
+//Animaciones al scroll
+
+const observador = new IntersectionObserver((entradas) => {
+    entradas.forEach (entrada => {
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add ('visible');
+            
+            observador.unobserve(entrada.target);
+        }
+    });
+}, {
+    threshold: 0.15
+})
+
+const elementosOcultos = document.querySelectorAll('.fade-up');
+
+elementosOcultos.forEach((elemento) => {
+    observador.observe(elemento);
+})
+
+//Mensaje de exito al enviar formulario
+
+const formulario = document.getElementById('contact-form');
+const mensajeExito = document.getElementById('mensaje-exito');
+
+formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const btnSubmit = formulario.querySelector('button[type="submit"]');
+    const textoOriginal = btnSubmit.innerHTML;
+    btnSubmit.innerHTML = 'Enviando solicitud... ⏳';
+    btnSubmit.style.opacity = '0.7';
+
+    setTimeout(() => {
+        formulario.style.display = 'none';
+        mensajeExito.classList.remove('oculto');
+
+        const datos = new FormData(formulario);
+        console.log("Nombre:", datos.get('nombre'));
+        console.log("Teléfono:", datos.get('telefono'));
+        console.log("Prioridad:", datos.get('servicio'));
+    }, 2000);
+});
